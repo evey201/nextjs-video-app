a9299fd49ad82e0763f5528f7e0dc826cdb9b01b
Eveshonyanime Ocheche-Alabi <evesho.alabi@gmail.com>
2022-11-04 14:51:52 +0200

    project setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import { Roboto_Slab } from '@next/font/google'
+
+const roboto_slab = Roboto_Slab({
+  subsets: ['latin'],
+  weight: '400'
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return  (
+    <main className={roboto_slab.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
 
 export default MyApp
