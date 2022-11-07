9ff1af0834fda22fb5dbd0397c1fb2cc7450df43
Eveshonyanime Ocheche-Alabi <evesho.alabi@gmail.com>
2022-11-07 17:41:14 +0200

    chore: working on banner component

---
+++ components/banner/Banner.jsx
@@ -0,0 +1,39 @@
+import styles from "./Banner.module.css";
+
+export const Banner = ({ title, subTitle, imgUrl }) => {
+  const handleOnPlay = () => {
+    console.log("Play");
+  };
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.leftWrapper}>
+          <div className={styles.leftElement}>
+            <div className={styles.nseriesWrapper}>
+                <p className={styles.firstLetter}>N</p>
+                <p className={styles.series}>S E R I E S</p>
+            </div>
+            <h3 className={styles.title}>{title}</h3>
+            <h3 className={styles.subTitle}>{subTitle}</h3>
+          </div>
+          <div className={styles.playBtnWrapper}>
+            <button className={styles.btnWithIcon} onClick={handleOnPlay}>
+              <span className={styles.playText}>Play</span>
+            </button>
+          </div>
+        </div>
+        <div
+          className={styles.bannerImg}
+          style={{
+            backgroundImage: `url(${imgUrl})`,
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            backgroundSize: "cover",
+            backgroundPosition: "50% 50%",
+          }}
+        ></div>
+      </div>
+    </>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Banner } from "../components";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -11,6 +12,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Netflix</h1>
+      <Banner
+        title="Clifford"
+        subTitle="a very big red dog"
+        imgUrl="/static/clifford.webp"
+      />
     </div>
-  )
+  );
 }
